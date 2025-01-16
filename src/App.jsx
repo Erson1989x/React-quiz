7d05cd481db61146e6bb7ebe0d6d8f166c7ca29b
Erhard Nagy <Erson1989x@gmail.com>
2025-01-16 18:52:44 +0200

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,59 @@
 
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 
+const initialState = {
+  questions: [],
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0
+}
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready'
+      }
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error'
+      }
+
+    default:
+      throw new Error('Unknown action')
+
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+
+useEffect(() => {
+  const fetchQuestions = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/questions')
+      const data = await response.json()  
+      dispatch({type: 'dataReceived', payload: data})  
+    }
+    catch (error) {
+      dispatch({type: 'dataFailed'})
+    }
+
+
+  }
+  fetchQuestions()
+}, [])
+
+
   return (
     <div className='app'>
       < Header />
