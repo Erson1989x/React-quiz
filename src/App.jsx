6a26666804dd5683b2ae9b1e1e778f3edfc06ad0
Erhard Nagy <Erson1989x@gmail.com>
2025-01-29 13:31:05 +0200

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Question from "./components/Question/Question";
 import NextButton from "./components/NextButton/NextButton";
 import Progress from "./components/Progress/Progress";
 import FinishScreen from "./components/FinishScreen/FinishScreen";
+import Footer from "./components/Footer/Footer";
+import Timer from "./components/Timer/Timer";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -16,6 +20,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 const reducer = (state, action) => {
@@ -35,6 +40,7 @@ const reducer = (state, action) => {
       return {
         ...state,
         status: "active",
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -68,6 +74,13 @@ const reducer = (state, action) => {
         index: 0,
         answer: null,
         points: 0,
+        secondsRemaining: null
+      };
+    case "tick":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status
       }
 
     default:
@@ -76,7 +89,7 @@ const reducer = (state, action) => {
 };
 
 const App = () => {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -124,12 +137,15 @@ const App = () => {
               questions={questions[index]}
               points={points}
             />
+            <Footer>
+            <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
             <NextButton
               index={index}
               numberQuestions={numberQuestions}
               dispatch={dispatch}
               answer={answer}
             />
+            </Footer>
           </>
         )}
         {status === "finished" && (
